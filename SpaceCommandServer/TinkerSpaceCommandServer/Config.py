8b31c49eb21b7d398c257773bd3987094207c339
Keith Hughes <keith.hughes@tinkermill.org>
2018-02-18 10:42:35 -0700

    Sensor heartbeat and value update time limits now configurable. Put config field names into Config.py

---
+++ SpaceCommandServer/TinkerSpaceCommandServer/Config.py
@@ -0,0 +1,41 @@
+#
+# This file contains constants and methods for configurations in the 
+# TinkerSpaceCommand server.
+#
+
+CONFIG_NAME_EXTERNAL_ID = "externalId"
+
+CONFIG_NAME_NAME = "name"
+
+CONFIG_NAME_DESCRIPTION = "description"
+
+CONFIG_NAME_MEASUREMENT_TYPE = "measurementType"
+
+CONFIG_NAME_MEASUREMENT_UNIT = "measurementUnit"
+
+CONFIG_NAME_SENSOR_ID = "sensorId"
+
+CONFIG_NAME_SENSED_ID = "sensedId"
+
+CONFIG_NAME_CHANNEL_IDS = "channelIds"
+
+CONFIG_VALUE_CHANNEL_IDS_WILDCARD = "*"
+
+# The character used to split channel IDs in a channel ID's field
+CONFIG_VALUE_CHANNEL_IDS_SPLIT = ":"
+
+CONFIG_NAME_SENSOR_DETAILS = "sensorDetails"
+
+CONFIG_NAME_SENSOR_DETAIL = "sensorDetail"
+
+CONFIG_NAME_CHANNELS = "channels"
+
+CONFIG_NAME_SENSOR_ASSOCIATIONS = "sensorAssociations"
+
+CONFIG_NAME_SENSORS = "sensors"
+
+CONFIG_NAME_PHYSICAL_LOCATIONS = "physicalLocations"
+
+CONFIG_NAME_SENSOR_UPDATE_TIME_LIMIT = "sensorUpdateTimeLimit"
+
+CONFIG_NAME_SENSOR_HEARTBEAT_TIME_LIMIT = "sensorHeartbeatTimeLimit"
