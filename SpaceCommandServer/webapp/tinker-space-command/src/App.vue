67809192f99c4e1e2a92d22c9d2413433306ef81
Keith Hughes <kmhughes@users.noreply.github.com>
2018-07-22 10:57:39 -0600

    Add in basic pages and routing.

--- SpaceCommandServer/webapp/tinker-space-command/src/App.vue
+++ SpaceCommandServer/webapp/tinker-space-command/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>Tinker Space Command</h1>
+
+    <div class="menu">
+      <ul>
+        <li><router-link to="/locations">Locations</router-link></li>
+        <li><router-link to="/sensors">Sensors</router-link></li>
+      </ul>
+    </div>
+
     <router-view/>
   </div>
 </template>
@@ -16,8 +24,32 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 1em;
 }
+
+div.menu > ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+div.menu > ul > li {
+  float: left;
+}
+
+div.menu > ul > li > a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+div.menu > ul > li > a:hover {
+  background-color: #111;
+}
+
 </style>
--- SpaceCommandServer/webapp/tinker-space-command/src/router/index.js
+++ SpaceCommandServer/webapp/tinker-space-command/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import Sensors from '@/components/Sensors'
+import Sensor from '@/components/Sensor'
+import Locations from '@/components/Locations'
+import Location from '@/components/Location'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/sensors',
+      name: 'Sensors',
+      component: Sensors
+    },
+    {
+      path: '/sensor/:id',
+      name: 'Sensor',
+      component: Sensor
+    },
+    {
+      path: '/locations',
+      name: 'Locations',
+      component: Locations
+    },
+    {
+      path: '/location/:id',
+      name: 'Location',
+      component: Location
     }
   ]
 })
